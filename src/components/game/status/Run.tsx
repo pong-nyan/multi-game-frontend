9ef937d4dbb956e464e174d1bcd1f49336db928a
yeonseong <yeonseong.dev@gmail.com>
2023-06-10 18:32:22 +0900

    WIP:

--- src/components/game/status/Run.tsx
+++ src/components/game/status/Run.tsx
@@ -1,39 +1,34 @@
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
-import Block from "../../../objects/Block";
 import Ball from "../../../objects/Ball";
-
+import { Server as SocketIOServer } from "socket.io";
+import { NextApiResponse } from "next";
 
 export default function Run({ setGameStatus }: { setGameStatus: Dispatch<SetStateAction<number>> }) {
 	const canvasRef = useRef<HTMLCanvasElement>(null)
 	const ctxRef = useRef<CanvasRenderingContext2D | null>()
 	const ballRef = useRef<Ball>()
-	const blocksRef = useRef<Block[]>()
 	const requestRef = useRef<number>()
 
 	const initGame = () => {
 		if (!canvasRef.current) return
 		const ctx = canvasRef.current.getContext('2d')
 		ctxRef.current = ctx
-		ballRef.current = new Ball(canvasRef.current.width, canvasRef.current.height, 10, 5)
-		blocksRef.current = [new Block(20, 60, 100, 100)]
-		// width: number, height: number, x: number, y: number, color?: string
+		// TODO: connet socket
+
 	}
 
 	const renderCanvas = () => {
-		if (!ballRef.current || !canvasRef.current || !ctxRef.current || !blocksRef.current) return
+		if (!ballRef.current || !canvasRef.current || !ctxRef.current) return
 		const ctx = ctxRef.current
 		ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
-		ballRef.current.draw(ctx, canvasRef.current.width , canvasRef.current.height, blocksRef.current[0])
-		blocksRef.current[0].draw(ctx)
-		console.log("hahahaha")
+		ballRef.current.draw(ctx);
 		requestRef.current	= requestAnimationFrame(renderCanvas)
-
 	}
+
 	useEffect(() => {
 		initGame()
 		requestRef.current	= requestAnimationFrame(renderCanvas)
 
-		console.log("hahahaha22")
 		return () => {
 			if (!requestRef.current) return
 			cancelAnimationFrame(requestRef.current)
@@ -41,7 +36,8 @@ export default function Run({ setGameStatus }: { setGameStatus: Dispatch<SetStat
 	})
 
 	return (
-		<canvas ref={canvasRef}>
+		// TODO: width and height should be defined in a config file
+		<canvas width={1280} height={720} ref={canvasRef}>
 			Canvas
 		</canvas>
 	)
---
+++ src/objects/Ball.ts
@@ -0,0 +1,20 @@
+import { Drawable } from '../interfaces/Drawable';
+
+export default class Ball implements Drawable {
+    x: number;
+    y: number;
+    radius: number;
+    color: string;
+    constructor(x: number, y: number, radius: number, color: string) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
