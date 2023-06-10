d6d3b8539191c7d09a6eb95fe803a53e9a02e662
jhmin95 <tesla52@naver.com>
2023-06-10 20:04:05 +0900

    FEAT: add KeyDownEvent
    free EventListener

--- src/components/game/status/Run.tsx
+++ src/components/game/status/Run.tsx
@@ -2,24 +2,28 @@ import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import Ball from "../../../objects/Ball";
 import { Server as SocketIOServer } from "socket.io";
 import { NextApiResponse } from "next";
-import {io as socketIOClient} from "socket.io-client";
+import { io as socketIOClient, Socket } from "socket.io-client";
+import { getEventListeners } from "events";
+import { DefaultEventsMap } from "@socket.io/component-emitter";
 
 export default function Run({ setGameStatus }: { setGameStatus: Dispatch<SetStateAction<number>> }) {
 	const canvasRef = useRef<HTMLCanvasElement>(null)
 	const ctxRef = useRef<CanvasRenderingContext2D | null>()
 	const ballRef = useRef<Ball>()
 	const requestRef = useRef<number>()
+	const socketRef = useRef<Socket<DefaultEventsMap, DefaultEventsMap>>()
 
+	const sendGameEvent = (e: KeyboardEvent) => {
+		if (!socketRef.current) return
+		socketRef.current.emit('game', { keyCode: e.key })
+	}
 	const initGame = () => {
 		console.log('init game all');
 		if (!canvasRef.current) return
 		const ctx = canvasRef.current.getContext('2d')
 		ctxRef.current = ctx
-		// TODO: connet socket
-		const socket = socketIOClient('http://localhost:4242')
-		socket.emit('game', () => {
-			console.log('game');
-		})
+		socketRef.current = socketIOClient('http://localhost:4242')
+		window.addEventListener('keydown', sendGameEvent)
 	}
 
 	const renderCanvas = () => {
@@ -27,16 +31,17 @@ export default function Run({ setGameStatus }: { setGameStatus: Dispatch<SetStat
 		const ctx = ctxRef.current
 		ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
 		ballRef.current.draw(ctx);
-		requestRef.current	= requestAnimationFrame(renderCanvas)
+		requestRef.current = requestAnimationFrame(renderCanvas)
 	}
 
 	useEffect(() => {
 		initGame()
-		requestRef.current	= requestAnimationFrame(renderCanvas)
+		requestRef.current = requestAnimationFrame(renderCanvas)
 
 		return () => {
 			if (!requestRef.current) return
 			cancelAnimationFrame(requestRef.current)
+			window.removeEventListener('keydown', sendGameEvent)
 		}
 	})
 
